579fa00ecf032a2fb791d498f5dec5c695d4835d
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2024-01-24 14:51:25 -0300

    02 tests

--- src/Domain/Entity/route.entity.spec.ts
+++ src/Domain/Entity/route.entity.spec.ts
@@ -1,8 +1,7 @@
-import { Route, RoutePublicProps } from "./route.entity"
+import { Coords, Route, RoutePublicProps } from "./route.entity"
 
 describe('Route Test', () => {
-    // Testando o comportamento
-    test('constructor', () => {
+    test('Route constructor', () => {
         const RouteNoPoints: RoutePublicProps = {
             title: 'Rota 1',
             startPosition: { latitute: 15, longitude: 16 },
@@ -29,8 +28,7 @@ describe('Route Test', () => {
         expect(route2.props).toStrictEqual(RouteWithPoints)
     })
 
-    // Testando métodos sobre título
-    test('methods title', () => {
+    test('Method updateTitle', () => {
         const RouteWithPoints: RoutePublicProps = {
             title: 'Rota 2',
             startPosition: { latitute: 15, longitude: 16 },
@@ -45,6 +43,44 @@ describe('Route Test', () => {
         const route = new Route(RouteWithPoints)
         route.updateTitle('Update title')
         expect(route.gTitle).toBe('Update title')
+    })
 
+    test('Method updatePosition', () => {
+        const RouteWithPoints: RoutePublicProps = {
+            title: 'Rota 3',
+            startPosition: { latitute: 15, longitude: 16 },
+            endPosition: { latitute: 25, longitude: 26 },
+            points: [
+                { latitute: 15, longitude: 16 },
+                { latitute: 20, longitude: 22 },
+                { latitute: 25, longitude: 26 }
+            ]
+        }
+
+        const route = new Route(RouteWithPoints)
+        let startPosition = { latitute: 17, longitude: 120 }
+        let endPosition = { latitute: 25, longitude: 30 }
+
+        route.updatePosition(startPosition, endPosition)
+        expect(route.gStartPosition).toBe(startPosition)
+        expect(route.gEndPosition).toBe(endPosition)
+    })
+
+    test('Methods updatePoints', () => {
+        const RouteNoPoints: RoutePublicProps = {
+            title: 'Rota 4',
+            startPosition: { latitute: 15, longitude: 16 },
+            endPosition: { latitute: 25, longitude: 26 },
+            points: []
+        }
+
+        const route = new Route(RouteNoPoints)
+        let points: Coords[] = [
+            { latitute: 15, longitude: 16 }
+        ]
+        let lenght: number = points.length
+        route.updatePoints(points)
+        expect(route.gPoints).toHaveLength(lenght)
+        expect(route.gPoints).toStrictEqual(points)
     })
 })

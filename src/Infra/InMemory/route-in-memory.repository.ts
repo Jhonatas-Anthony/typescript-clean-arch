b44f1a96359d7dd99199ae7274f70b09ef2f12f6
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2024-01-24 15:58:17 -0300

    01 infra

---
+++ src/Infra/InMemory/route-in-memory.repository.ts
@@ -0,0 +1,9 @@
+import { Route } from "../../Domain/Entity/route.entity";
+import { RouteRepositoryInterface } from "../../Domain/Repositories/route.repository";
+
+export class RouteInMemory implements RouteRepositoryInterface {
+    items: Route[] = []
+    async insert(route: Route): Promise<void> {
+        this.items.push(route)
+    }
+}
